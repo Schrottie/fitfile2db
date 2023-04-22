0ab4a5aab7f70ba37a2638030b9c801ce2733bf7
Maik Bischoff <schrottie@gmail.com>
2023-04-22 14:45:12 +0200

    Update prepare_postgre.sql

--- prepare_postgre.sql
+++ prepare_postgre.sql
@@ -1,14 +1,14 @@
 CREATE TABLE fitfile_userdata (
-    id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY
 );
 
 CREATE TABLE fitfile_data (
-    id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY
     filename VARCHAR(MAX) NOT NULL
 );
 
 CREATE TABLE fitfile_totals (
-    id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY
 );
 
 CREATE TABLE known_fitfiles (
